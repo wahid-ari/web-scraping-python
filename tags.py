4473c07f8f9a5c63e3e58b188a7cb949e89374d8
wahidari <wahiid.ari@gmail.com>
2023-05-22 19:21:20 +0700

    create tags scraping

---
+++ tags.py
@@ -0,0 +1,72 @@
+import bs4
+import json
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+my_url = 'https://www.goodreads.com/quotes'
+
+# opening dan connection to website
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# graps the product
+right_container = page_soup.find(
+    "div", {"class": "rightContainer"})
+tags_container = right_container.find(
+    "div", {"class": "bigBoxContent containerWithHeaderContent"})
+# print(right_container)
+all_tags = tags_container.find_all("a", {"class": "gr-hyperlink"})
+# print(all_tags)
+# prepare CSV
+file = "tags.csv"
+header = "id, name, link \n"
+f = open(file, "w", encoding="utf-8")
+f.write(header)
+
+index = 1
+
+tags_json = []
+
+for tags in all_tags:
+    name = tags.text.replace("Quotes", '').strip()
+    link = tags.get("href")
+    # books = td.a.select("span")
+    # title = books[0].text.replace("á", "a").replace("í", "i")
+    # authors = td.div.select("a")
+    # author = authors[0].text.replace('é', 'e')
+    # author_link = td.div.select("a")[0].get("href")
+    # ratings = td.findAll("span", {"class": "greyText smallText uitext"})
+    # rating = ratings[0].text.strip().replace(",", ".").replace("—", "-")
+    # scores = td.findAll("div", {"style": "margin-top: 5px"})
+    # score = scores[0].span.a.text.strip().replace(
+    #     "score:", "").replace(",", ".").replace(" ", "")
+    # voted = scores[0].span.select("a:nth-child(3)")[0].text.replace(
+    #     "people voted", "").replace("person voted", "").strip()
+
+    print("index: ", index)
+    print("name: " + name)
+    print("link: " + "https://www.goodreads.com" + link)
+    print("\n")
+
+    if (name != ''):
+        tags_json.append({
+            "id": index,
+            "name": name,
+            "link": "https://www.goodreads.com" + link
+        })
+        f.write(
+            str(index) + ", " +
+            name + ", " +
+            "https://www.goodreads.com" + link +
+            "\n"
+        )
+
+        index = index + 1
+
+with open("tags.json", "w") as outfile:
+    json.dump(tags_json, outfile, indent=4)
+
+f.close()
